ca9246fac47dd9c06b96187e4e4e4547ebb1440b
Hardik <hgarg1_be21@thapar.edu>
2024-06-06 14:30:10 +0530

    All files added

---
+++ hello/src/test/java/com/hardik/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
